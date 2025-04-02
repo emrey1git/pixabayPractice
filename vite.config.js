a0869d76ca7e847d30bbf7d74f418e9a0fd7b97f
emre.yigit <emreygt01@icloud.com>
2025-04-02 10:17:55 +0300

    try

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,18 @@ import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
+import postcssPresetEnv from 'postcss-preset-env';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/find-photo/',
+    base: '/find-photo/',  // GitHub Pages için base URL
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
     build: {
       sourcemap: true,
+      cssCodeSplit: true,  // CSS dosyalarını bölüp optimize eder
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
@@ -21,29 +22,25 @@ export default defineConfig(({ command }) => {
               return 'vendor';
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: '[name].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
         },
       },
       outDir: '../dist',
       emptyOutDir: true,
     },
+    css: {
+      postcss: {
+        plugins: [
+          postcssPresetEnv({
+            stage: 1,
+          }),
+        ],
+      },
+    },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
+      FullReload(['./src/**/*.html']),
     ],
   };
 });
